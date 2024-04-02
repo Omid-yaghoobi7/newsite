447ab730a1eba1206eb8c8cfe967be54d18ce250
Omid-yaghoobi7 <150296011+Omid-yaghoobi7@users.noreply.github.com>
2024-04-03 02:04:13 +0330

    Create more template tags
    Create more template tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,18 @@
 from django import template
+from blog.models import Post, Category
 
 register = template.Library()
+
+@register.inclusion_tag('blog/blog-popular-posts.html')
+def latestposts():
+    posts =Post.objects.filter(status=1).order_by('published_date')[:3]
+    return {'posts': posts}
+
+@register.inclusion_tag('blog/blog-category.html')
+def postcategories():
+    posts =Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+    return {'categories':cat_dict}
