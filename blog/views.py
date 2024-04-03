e04dd5921b1bf51bfceaf28a8bf769a4470c02ba
Omid-yaghoobi7 <150296011+Omid-yaghoobi7@users.noreply.github.com>
2024-04-04 02:46:47 +0330

    Search and Pagination
    Search and Pagination

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 # Create your views here.
-def blog_view(request):
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(status=1)
+    if kwargs.get('cat_name') !=None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') !=None:
+        posts = posts.filter(author__username=kwargs['author_username'])
+    posts = Paginator(posts, 3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -17,3 +31,16 @@ def test(request, pid):
     post = get_object_or_404(Post, pk=pid)
     context = {'post': post}
     return render(request, 'blog/test.html', context)
+
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        posts = posts.filter(content__contains=request.GET.get('s'))
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
+
+#def blog_category(request, cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
