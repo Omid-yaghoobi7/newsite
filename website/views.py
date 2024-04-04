0458b3f3c0dd22bdc2c1bcf48d4f21612268c369
Omid-yaghoobi7 <150296011+Omid-yaghoobi7@users.noreply.github.com>
2024-04-05 01:54:06 +0330

    Forms and Modelforms
    Forms and Modelforms

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from website.forms import NameForm, ContactForm, NewsletterForm
+from django.contrib import messages
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -8,4 +10,34 @@ def about_view(request):
     return render(request, 'website/about.html')   
 
 def contact_view(request):
-    return render(request, 'website/contact.html')    
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Your message has been sent successfully')
+        else:
+            messages.add_message(request, messages.ERROR, 'There was an error')
+
+    form = ContactForm()
+    return render(request, 'website/contact.html', {'form': form})
+
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
+
+
+def test_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('success')
+        else:
+            return HttpResponse('error')
+    form = ContactForm()
+    return render(request, 'website/test.html', {'form': form})
