6bb1790993f864da84d14acf50463170ca36dae6
Omid-yaghoobi7 <150296011+Omid-yaghoobi7@users.noreply.github.com>
2024-04-09 02:11:11 +0330

    Commenting section
    Commenting section

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post, Category
+from blog.models import Post, Category, Comment
 
 register = template.Library()
 
@@ -7,6 +7,10 @@ register = template.Library()
 def latestposts():
     posts =Post.objects.filter(status=1).order_by('published_date')[:3]
     return {'posts': posts}
+
+@register.simple_tag(name='comments_counter')
+def function(pid):
+    return Comment.objects.filter(post=pid, approved=True).count()
 
 @register.inclusion_tag('blog/blog-category.html')
 def postcategories():
